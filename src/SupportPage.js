4fa90abbabd4cc2bc5919037c1ba221fe4648acd
Judson Stevens <Judson.Stevens@gmail.com>
2018-07-21 17:08:31 -0600

    Fixes background images and reworks styling

--- src/SupportPage.js
+++ src/SupportPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { Container, Header, Grid, Card, Form, Button, Checkbox, Image } from 'semantic-ui-react'
-import './support.css'
+import { Container, Header, Grid, Card, Button, Image } from 'semantic-ui-react'
+import './SupportPage.css'
 
 class SupportPage extends Component {
   constructor(props) {
@@ -22,89 +22,93 @@ class SupportPage extends Component {
 
   render() {
     return (
-      <div class='support-body-div'>
-            <div>
-              <h1>Support Survivor Connect</h1>
-            </div>
-        <Grid container columns={3}>
+      <div class="support-body-div">
+        <Grid centered container>
           <Grid.Row centered>
-            <Grid.Column id='div_column_support'>
-              <Card.Group container columns={2}>
-                <Card id='donate_card'>
-                  <Card.Content>
-                  <Image floated='right' size='mini' src={require("./heart.png")} />
-                    <Card.Header>
-                      Donate
-                    </Card.Header>
-                    <Card.Meta>
-                      Give securely via PayPal
-                    </Card.Meta>
-                    <Card.Description>
-                      Your generosity helps us maintain this site and help survivors.
-                    </Card.Description>
-                  </Card.Content>
-                    <Card.Content extra>
-                  <div className='ui buttons'>
-                    <Button basic color='black'>
-                      Donate with PayPal
-                    </Button>
-                  </div>
-                </Card.Content>
-                </Card>
-                <Card id='advocate_card'>
-                  <Card.Content>
-                    <Image floated='right' size='mini' src={require("./law.png")} />
-                    <Card.Header>Advocate</Card.Header>
-                    <Card.Meta>We are always looking for legal professionals to add to our network.</Card.Meta>
-                    <Card.Description>
-                      Sign up to offer pro-bono hours to those in need of legal assistance.
-                    </Card.Description>
-                  </Card.Content>
-                  <Card.Content extra>
+            <Grid.Column id="inset-column">
+              <Container id="support-container">
+                <Header id="support-header">
+                  Suport Survivor Connect
+                </Header>
+                <Card.Group container columns={2}>
+                  <Card id='donate_card'>
+                    <Card.Content>
+                    <Image floated='right' size='mini' src={require("./heart.png")} />
+                      <Card.Header>
+                        Donate
+                      </Card.Header>
+                      <Card.Meta>
+                        Give securely via PayPal
+                      </Card.Meta>
+                      <Card.Description>
+                        Your generosity helps us maintain this site and help survivors.
+                      </Card.Description>
+                    </Card.Content>
+                      <Card.Content extra>
                     <div className='ui buttons'>
                       <Button basic color='black'>
-                        Request a law practice profile
+                        Donate with PayPal
                       </Button>
                     </div>
                   </Card.Content>
-                </Card>
-                <Card id='contact_card'>
-                  <Card.Content>
-                    <Image floated='right' size='mini' src={require("./contact.png")}/>
-                    <Card.Header>Contact</Card.Header>
-                    <Card.Meta>Send us a message or join our mailing list</Card.Meta>
-                    <Card.Description>
-                      We are looking forward to hearing from you!
-                    </Card.Description>
-                  </Card.Content>
-                  <Card.Content extra>
-                    <div className='ui two buttons'>
-                      <Button basic color='black'>
-                        Get in touch
-                      </Button>
-                    </div>
-                  </Card.Content>
-                </Card>
-                <Card id='share_card'>
-                  <Card.Content>
-                    <Image floated='right' size='mini' src={require("./social-media.png")} />
-                    <Card.Header>Share</Card.Header>
-                    <Card.Meta>By sharing our app, you're helping us reach more survivors and more service providers </Card.Meta>
-                    <Card.Description>Click below to share Survivor Connect on social media.</Card.Description>
-                  </Card.Content>
-                  <Card.Content extra>
-                    <div className='ui two buttons'>
-                      <Button basic color='black'>
-                        Share
-                      </Button>
-                    </div>
-                  </Card.Content>
-                </Card>
-            </Card.Group>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </div>
+                  </Card>
+                  <Card id='advocate_card'>
+                    <Card.Content>
+                      <Image floated='right' size='mini' src={require("./law.png")} />
+                      <Card.Header>Advocate</Card.Header>
+                      <Card.Meta>We are always looking for legal professionals to add to our network.</Card.Meta>
+                      <Card.Description>
+                        Sign up to offer pro-bono hours to those in need of legal assistance.
+                      </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <div className='ui buttons'>
+                        <Button basic color='black'>
+                          Request a law practice profile
+                        </Button>
+                      </div>
+                    </Card.Content>
+                  </Card>
+                  <Card id='contact_card'>
+                    <Card.Content>
+                      <Image floated='right' size='mini' src={require("./contact.png")}/>
+                      <Card.Header>Contact</Card.Header>
+                      <Card.Meta>Send us a message or join our mailing list</Card.Meta>
+                      <Card.Description>
+                        We are looking forward to hearing from you!
+                      </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <div className='ui two buttons'>
+                        <Button basic color='black'>
+                          Get in touch
+                        </Button>
+                      </div>
+                    </Card.Content>
+                  </Card>
+                  <Card id='share_card'>
+                    <Card.Content>
+                      <Image floated='right' size='mini' src={require("./social-media.png")} />
+                      <Card.Header>Share</Card.Header>
+                      <Card.Meta>By sharing our app, you're helping us reach more survivors and more service providers </Card.Meta>
+                      <Card.Description>Click below to share Survivor Connect on social media.</Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                      <div className='ui two buttons'>
+                        <Button basic color='black'>
+                          Share
+                        </Button>
+                      </div>
+                    </Card.Content>
+                  </Card>
+                </Card.Group>
+              </Container>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+          </Grid.Row>
+        </Grid>
+      </div>
         );
     }
 }
