dd02e2317f0a0e762935cb4b89a6a4f7c4ad2e1b
JamisonOrdway <jamisonordway@gmail.com>
2018-07-24 06:57:25 -0600

    merge conflict stuff

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,11 +4,7 @@ import Search from './Search'
 import NotFound from './NotFound'
 import SupportPage from './SupportPage'
 import SearchResults from './SearchResults'
-<<<<<<< HEAD
-import Contact from './Contact'
-=======
 import LawyerRegister from './LawyerRegister'
->>>>>>> 96ba1ab2893a6c3d34359ee7f2d2f12957e25f12
 
 class App extends Component {
   render() {
@@ -19,11 +15,7 @@ class App extends Component {
           <Route path='/index.html' exact component={Search} />
           <Route path='/search_results' exact component={SearchResults} />
           <Route path='/support' exact component={SupportPage} />
-<<<<<<< HEAD
-          <Route path='/contact' exact component={Contact} />
-=======
           <Route path='/lawyer_register' exact component={LawyerRegister} />
->>>>>>> 96ba1ab2893a6c3d34359ee7f2d2f12957e25f12
           <Route component={NotFound} />
         </Switch>
       </Router>
