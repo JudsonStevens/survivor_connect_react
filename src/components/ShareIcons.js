8902641f9fd938c65fd3f39a9f2111e22507a504
JamisonOrdway <jamisonordway@gmail.com>
2018-07-30 08:40:24 -0600

    finishes styling for share page

--- src/components/ShareIcons.js
+++ src/components/ShareIcons.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 import { Container, Grid } from 'semantic-ui-react'
 import { FacebookIcon, TwitterIcon,} from 'react-share';
+import '../css/Share.css'
 
 class ShareIcons extends Component {
   render() {
     return (
       <div className='share-icons-div'>
-        <Grid centered Container>
-          <Grid.Row stretched centered>
-            <Grid.Column id='inset-column'>
-            <TwitterIcon size={32} round={true} />
-            <FacebookIcon size={32} round={true} />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+        <h2>Share Survivor Connect</h2>
+        <TwitterIcon className='twitter' size={92} round={false} />
+        <FacebookIcon className='facebook' size={92} round={false} />
       </div>
     )
   }
--- src/containers/Share.js
+++ src/containers/Share.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, Grid } from 'semantic-ui-react';
 import ShareIcons from '../components/ShareIcons'
 import '../css/Share.css'
 
 class Share extends Component {
   render() {
     return (
-      <Container className="share-page">
-      <ShareIcons />
-      </Container>
+      <span className="share-page">
+        <Grid centered Container>
+          <Grid.Row stretched centered>
+            <Grid.Column id='inset-column'>
+              <ShareIcons />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>      
+      </span>
     )
   }
 }
