4dba6eef976f581f45c68838c11b62fb7ea4f465
JamisonOrdway <jamisonordway@gmail.com>
2018-07-29 21:08:39 -0600

    Adds facebook and twitter button

--- src/components/ShareIcons.js
+++ src/components/ShareIcons.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react'
-import { FacebookShareButton, TwitterShareButton,} from 'react-share';
+import { Container, Grid } from 'semantic-ui-react'
+import { FacebookIcon, TwitterIcon,} from 'react-share';
 
 class ShareIcons extends Component {
   render() {
@@ -9,6 +9,8 @@ class ShareIcons extends Component {
         <Grid centered Container>
           <Grid.Row stretched centered>
             <Grid.Column id='inset-column'>
+            <TwitterIcon size={32} round={true} />
+            <FacebookIcon size={32} round={true} />
             </Grid.Column>
           </Grid.Row>
         </Grid>
--- src/containers/Share.js
+++ src/containers/Share.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
 import ShareIcons from '../components/ShareIcons'
+import '../css/Share.css'
 
 class Share extends Component {
   render() {
     return (
-      <Container className="share=page">
+      <Container className="share-page">
       <ShareIcons />
       </Container>
     )
