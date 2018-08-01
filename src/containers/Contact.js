94ccaf97d9a513f7fd242f3c016432af8420ab4d
JamisonOrdway <jamisonordway@gmail.com>
2018-08-01 09:44:11 -0600

    removes footers from other pages

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -18,24 +18,6 @@ class Contact extends Component {
             </Grid.Column>
           </Grid.Row>
         </Grid>
-        <div className='contact-footer'>
-            <div className='contact-signal-blurb'>
-              <p>We recommend that all users of our site communicate with attorneys 
-              via Signal,</p>
-              <p>a free and secure app which encrypts all messages and calls when both parties use it.</p>
-              <p>Find a link{' '} <a href="https://signal.org/">here for Signal.</a></p>
-            </div>
-            <div className="contact-credits">
-              <p>Image credits:{'   '}
-              <a href="https://unsplash.com/@saffu">Saffu</a>,{'  '}
-              <a href="https://unsplash.com/@firmbee">William Iven</a>,{'  '} 
-              <a href="hhttps://unsplash.com/@kalljet">Katarzyna Grabowska</a>,{'  '} 
-              <a href="https://unsplash.com/@acharki95">Aziz Acharki</a>,{'  '}
-              <a href="https://unsplash.com/@sharonmccutcheon">Sharon McCutcheon</a>,{'  '} 
-              <a href="https://unsplash.com/@adrianpelletier">Adrian Pelletier</a>,{'  '} 
-              <a href="https://unsplash.com/@pichler_sebastian">Sebastian Pichler</a></p>{'  '}
-            </div>
-          </div>
       </div>
         );
     }
